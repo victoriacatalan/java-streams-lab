47029a046c8ca6d697c4003666dd577d816316d7
Victoria Catalán <victoria.catalan@omegapoint.se>
2020-10-08 22:43:14 +0200

    Add for loop version of solution

--- src/test/java/BookTest.java
+++ src/test/java/BookTest.java
@@ -4,9 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class BookTest {
@@ -30,51 +28,116 @@ public class BookTest {
                 }
             }
         }
-
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
     void Should_ReturnPriceOfAllFantasy() {
-        int expected = 248;
-        int actual = 1;
-
-        //TODO: Repleace actual with stream
-
+        int expected = 327;
+        int actual = 0;
+        for (Book book : library) {
+            if (book.getTags().contains("Fantasy")) {
+                actual += book.getPrice();
+            }
+        }
         Assertions.assertEquals(expected, actual);
-
     }
 
 
     @Test
     void Should_ReturnAllBooksByTolkien() {
-        List<Book> expected = Arrays.asList(BookFixture.getSilmarillon(), BookFixture.getLotr());
-        int expectedPrice = 246;
+        List<Book> expected = Arrays.asList(BookFixture.getLotr(), BookFixture.getSilmarillon());
+        int expectedPrice = 248;
 
-        List<Book> actual = null;
-        int actualPrice = 1;
+        List<Book> actual = new ArrayList<>();
+        int actualPrice = 0;
+        for (Book book : library) {
+            if (book.getAuthor().contains("J. R. R. Tolkien")) {
+                actual.add(book);
+                actualPrice += book.getPrice();
+            }
+        }
 
         Assertions.assertEquals(expected, actual);
         Assertions.assertEquals(expectedPrice, actualPrice);
     }
 
+    @Test
+    void Should_ReturnMostExpensiveBooks() {
+        Book expected = BookFixture.getOCP();
+        Book actual = library.get(0);
+        for (Book book : library) {
+            if (actual.getPrice() < book.getPrice()) {
+                actual = book;
+            }
+        }
+        Assertions.assertEquals(expected, actual);
+    }
 
     @Test
     void Should_ReturnTwoMostExpensiveBooks() {
         List<Book> expected = List.of(BookFixture.getOCP(), BookFixture.getInk());
         int expectedPrice = 667;
 
-        List<Book> actual = null;
-        int actualPrice = 1;
+        List<Book> actual = new ArrayList<>();
+        int actualPrice = 0;
 
         Assertions.assertEquals(expected, actual);
         Assertions.assertEquals(expectedPrice, actualPrice);
     }
 
+
     @Test
-    void Should_ReturnStatistics() {
-        library.stream()
-                .mapToInt(Book::getPrice)
-                .sum();
+    void Should_ReturnFirstStudyGuid() {
+        Book expected = BookFixture.getInk();
+
+        Book actual = library.get(0);
+        for(Book book : library) {
+            for(String tag : book.getTags()) {
+                if(tag.equals("Study Guide")) {
+                    actual = book;
+                    break;
+                }
+            }
+        }
+        Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void Should_GroupBooksByAuthor() {
+        Map<String, List<Book>> expected = BookFixture.mapByAuthor();
+
+        Map<String, List<Book>> actual = new HashMap<>();
+        for (Book book : library) {
+            if (actual.containsKey(book.getAuthor())) {
+                actual.get(book.getAuthor()).add(book);
+            } else {
+                ArrayList<Book> books = new ArrayList<>();
+                books.add(book);
+                actual.put(book.getAuthor(), books);
+            }
+        }
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void Should_GroupBooksByTags() {
+        Map<String, List<Book>> expected = BookFixture.mapByTag();
+
+        Map<String, List<Book>> actual = new HashMap<>();
+        for(Book book : library) {
+            for(String tag : book.getTags()) {
+                if(actual.containsKey(tag)) {
+                    actual.get(tag).add(book);
+                } else {
+                    ArrayList<Book> books = new ArrayList<>();
+                    books.add(book);
+                    actual.put(tag, books);
+                }
+            }
+        }
+
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
